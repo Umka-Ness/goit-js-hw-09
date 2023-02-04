6d910a152006d0248db4daa52ea04a35bea53aa2
Umka-Ness <62846545+Umka-Ness@users.noreply.github.com>
2023-02-04 04:08:55 +0000

    Deploying to gh-pages from @ Umka-Ness/goit-js-hw-09@8bf811f82dee10bec3e7caeb6aa3d57d37d290b1 🚀

---
+++ 01-color-switcher.c58567a3.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=null;e.disabled=!0,t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,n=setInterval((function(){document.body.style.background="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),e.addEventListener("click",(function(){clearInterval(n),e.disabled=!0,t.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.c58567a3.js.map
